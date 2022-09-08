84fd8b9915b0b441a5f6631c43c06aa50b007892
Doniyor <36728197+Doniyor4120@users.noreply.github.com>
2022-09-08 11:50:32 +0500

    feat: entity configuration

--- src/app/posts/state/posts.state.ts
+++ src/app/posts/state/posts.state.ts
@@ -1,8 +1,24 @@
 import {Post} from "../../models/post.model";
 import {createEntityAdapter, EntityState} from "@ngrx/entity";
 
-export interface PostsState extends EntityState<Post> {}
+export interface PostsState extends EntityState<Post> {
+  count: number;
+}
 
 export const postsAdapter = createEntityAdapter<Post>();
 
-export const initialState: PostsState = postsAdapter.getInitialState();
+export const initialState: PostsState = postsAdapter.getInitialState({
+  count: 0
+});
+
+export function sortByPost(a: Post, b: Post): number {
+  const compare = a.title.localeCompare(b.title);
+  if (compare > 0) {
+    return -1
+  }
+  if (compare < 0) {
+    return 1
+  }
+
+  return compare
+}
